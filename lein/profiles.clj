a87f04f6f2f86e67e9938dbfa8c8a39ce4e42bb9
Zachary Scott <mail@zzak.io>
2018-04-08 22:14:10 +0900

    Add user profile that includes clj-stacktrace

---
+++ lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user {:dependencies [[clj-stacktrace "0.2.8"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly (deref new))))]}}
