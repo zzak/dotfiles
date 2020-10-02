9d74065a1a6b753ac397ecdd3e96020ba24805da
Zachary Scott <mail@zzak.io>
2020-10-02 14:18:35 +0900

    set-key helm-show-kill-ring

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -99,6 +99,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
 (add-to-list 'auto-mode-alist '("\\.graphql\\'" . graphql-mode))
 
