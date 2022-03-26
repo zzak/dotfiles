5210f22b013b22701ef7139d21f0de185cd3a3c1
Zachary Scott <zzakscott@gmail.com>
2022-03-26 14:51:50 +0900

    Remove cider-nrepl from lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,3 @@
-{:repl {:plugins [[cider/cider-nrepl "0.25.2"]]}}
-
 {:user {:pedantic? :ranges
         :dependencies [[clj-stacktrace "0.2.8"]]
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
